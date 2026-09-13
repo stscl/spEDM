8eb72d24097254b438763ef86605b3e37435867c
SpatLyu <lyu.geosocial@gmail.com>
2026-09-14 00:00:01 +0800

    fully support spatial raster data with anisotropic embedding

--- R/embedded.R
+++ R/embedded.R
@@ -14,7 +14,7 @@
                                 detrend = FALSE, grid.coord = TRUE, embed.direction = 0){
   mat = .uni_grid(data,target,detrend,grid.coord)
   if (!stack) {
-    res = RcppGenGridEmbeddings(mat,E,tau,style)
+    res = RcppGenGridEmbeddings(mat,E,tau,style,embed.direction)
   } else {
     res = RcppGenGridEmbeddingsCom(mat,E,tau,style,embed.direction)
   }
